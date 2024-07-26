8e81f45b57312e05a786ed33b670eb4bc7d7b8b6
ABDULRAHMANW12 <146181043+ABDULRAHMANW12@users.noreply.github.com>
2024-07-26 14:34:43 +0300

    Add files via upload

---
+++ nodejs-bakaend-1/data10.js
@@ -0,0 +1,65 @@
+const fs = require("fs")
+
+const addPerson = (id , fname, lname, city, age) => {
+    const allData = loadeinfo()
+    const DuoplicatedData = allData.filter((item) => {
+        return item.id === id
+    })
+    if (DuoplicatedData.length === 0) {
+
+        allData.push({
+            id,
+            fname,
+            lname,
+            city,
+            age
+        }),
+            saveAlldata(allData)
+
+    } else {
+        console.log("ERORR DUOBLICATED DATA")
+    }
+}
+
+const loadeinfo = () => {
+    try {
+        const dataJson = fs.readFileSync("data10.json").toString()
+        return JSON.parse(dataJson)
+    }
+    catch {
+        return []
+    }
+}
+
+const saveAlldata = (allData) => {
+    const saveAlldataJson = JSON.stringify(allData)
+    fs.writeFileSync("data10.json", saveAlldataJson)
+}
+
+// delete data=======
+
+const deletePerson = (id) =>{
+    const allData = loadeinfo()
+    const deleteItem = allData.filter((item) => {
+        return item.id !== id
+    })
+    saveAlldata(deleteItem)
+    console.log(deleteItem)
+    console.lo("you have already deleted an item ")
+}
+
+// read Data========
+
+const catchData = (id) =>{
+    const allData = loadeinfo()
+    const searchedItem = allData.find((obj) =>{
+        return obj.id == id
+    })
+    console.log(searchedItem)
+}
+
+module.exports = {
+    addPerson,
+    deletePerson,
+    catchData
+}
